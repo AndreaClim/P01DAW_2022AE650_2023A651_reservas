39017da3aefbde031b72d54ea05c7b6487c97819
miche <miche@DESKTOP-EEADCHD>
2025-02-27 17:17:17 -0600

    Agregar archivos de proyecto.

---
+++ P01DAW_2022AE650_2023A651_reservas/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace P01DAW_2022AE650_2023A651_reservas
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
