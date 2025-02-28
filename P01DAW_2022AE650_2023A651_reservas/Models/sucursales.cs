2dcbb5748fcc30cbabc22d2a43e98a982c92f1dd
Amaya-2003 <caluchogamer@gmail.com>
2025-02-27 18:49:34 -0600

    commit clase sucuersales, usuarios modificada

---
+++ P01DAW_2022AE650_2023A651_reservas/Models/sucursales.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace L01_2022AE650_2023CA651.Models
+{
+    public class sucursales
+    {
+        [Key]
+        public int sucursales_id { get; set; }
+        public int administrador_id { get; set; }
+        public int usuario_id { get; set; }
+        public string nombre { get; set; }
+        public string direccion { get; set; }
+        public string telefono { get; set; }
+        public string administrador { get; set; }
+        public int numeroEspacios { get; set; }
+    }
+}
--- P01DAW_2022AE650_2023A651_reservas/Models/usuarios.cs
+++ P01DAW_2022AE650_2023A651_reservas/Models/usuarios.cs
@@ -1,12 +1,15 @@
-﻿namespace L01_2022AE650_2023CA651.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace L01_2022AE650_2023CA651.Models
 {
     public class usuarios
     {
-        public int usuarioId { get; set; }
+        [Key]
+        public int usuario_id { get; set; }
         public string nombre { get; set; }
         public string correo { get; set; }
         public string telefono { get; set; }
         public string contrasena { get; set; }
-        public string rol { get; set; } 
+        public string rol { get; set; } // "Cliente" o "Empleado"
     }
 }
