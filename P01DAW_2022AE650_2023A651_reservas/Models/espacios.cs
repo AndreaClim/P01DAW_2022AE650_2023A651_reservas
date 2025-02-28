3f68696060641b7fe5af5b9b0325db857c590643
miche <miche@DESKTOP-EEADCHD>
2025-02-27 18:19:59 -0600

    commit 5 models con clase espacios

---
+++ P01DAW_2022AE650_2023A651_reservas/Models/espacios.cs
@@ -0,0 +1,11 @@
+﻿namespace P01DAW_2022AE650_2023A651_reservas.Models
+{
+    public class espacios
+    {
+        public int id { get; set; }
+        public int numero { get; set; }
+        public string ubicacion { get; set; }
+        public double costo { get; set; }
+        public string estado { get; set; }
+    }
+}
