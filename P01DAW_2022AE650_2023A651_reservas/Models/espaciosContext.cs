1cd9a864e8cd7d1c0741ac2431f5d399cc7e6ba2
miche <miche@DESKTOP-EEADCHD>
2025-02-27 18:51:50 -0600

    COMMIT 10 ESPACIOS CONTEXT

---
+++ P01DAW_2022AE650_2023A651_reservas/Models/espaciosContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+namespace P01DAW_2022AE650_2023A651_reservas.Models
+{
+    public class espaciosContext : DbContext
+    {
+        public espaciosContext(DbContextOptions<espaciosContext> options) : base(options)
+        {
+        }
+        public DbSet<espacios> espacios { get; set; }
+    }
+}
