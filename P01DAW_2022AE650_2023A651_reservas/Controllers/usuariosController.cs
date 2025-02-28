782e3b903e8dbdc0994c18b3e747f317e9a2b44d
Amaya-2003 <caluchogamer@gmail.com>
2025-02-27 19:52:42 -0600

    commit usuarios controller

---
+++ P01DAW_2022AE650_2023A651_reservas/Controllers/usuariosController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using P01DAW_2022AE650_2023A651_reservas.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace P01DAW_2022AE650_2023A651_reservas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class usuariosController : ControllerBase
+    {
+        private readonly usuariosContext _usuariosContext;
+        public usuariosController(usuariosContext usuariosContext)
+        {
+            _usuariosContext = usuariosContext;
+        }
+    }
+}
