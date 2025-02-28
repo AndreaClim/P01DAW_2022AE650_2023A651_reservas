fae839c68ec47f0ad94f20e3c5b5f43c318d3ff3
miche <miche@DESKTOP-EEADCHD>
2025-02-27 20:45:35 -0600

    COMMIT 17 CLASE DE LOGINS

---
+++ P01DAW_2022AE650_2023A651_reservas/Models/LoginRequest.cs
@@ -0,0 +1,11 @@
+﻿using P01DAW_2022AE650_2023A651_reservas.Models;
+namespace P01DAW_2022AE650_2023A651_reservas.Models
+{
+    public class LoginRequest
+    {
+        public string Correo { get; set; }
+        public string Contrasena { get; set; }
+    }
+
+    
+}
