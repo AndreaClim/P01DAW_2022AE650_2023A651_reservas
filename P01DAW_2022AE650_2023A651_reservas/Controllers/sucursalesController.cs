9e35d7d1118b3e26bdf65c963693b5d4ab249c40
Amaya-2003 <caluchogamer@gmail.com>
2025-02-27 19:45:17 -0600

    commit sucursalescontroller

---
+++ P01DAW_2022AE650_2023A651_reservas/Controllers/sucursalesController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using P01DAW_2022AE650_2023A651_reservas.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace P01DAW_2022AE650_2023A651_reservas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class sucursalesController : ControllerBase
+    {
+        private readonly sucursalesContext _sucursalesContext;
+        public sucursalesController(sucursalesContext sucursalesContext)
+        {
+            _sucursalesContext = sucursalesContext;
+        }
+    }
+}
