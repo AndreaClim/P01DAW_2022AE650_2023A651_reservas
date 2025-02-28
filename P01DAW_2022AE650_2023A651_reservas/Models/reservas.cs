03054448acbaab320fb7d9eedefdbd343009443e
miche <miche@DESKTOP-EEADCHD>
2025-02-27 18:42:50 -0600

    commit 8 añadiendo clase reservas a models

---
+++ P01DAW_2022AE650_2023A651_reservas/Models/reservas.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace P01DAW_2022AE650_2023A651_reservas.Models
+{
+    public class reservas
+    {
+        public int id { get; set; }
+        public int id_espacios { get; set; }
+        public int id_usuario { get; set; }
+        public string fecha { get; set; }
+        public string horaI { get; set; }
+        public string horasR { get; set; }
+        public string estado { get; set; }
+    }
+}
