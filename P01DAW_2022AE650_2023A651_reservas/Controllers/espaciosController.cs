757b9c603f91a61c233e2cf31b6911d5b203bf82
miche <miche@DESKTOP-EEADCHD>
2025-02-27 19:44:19 -0600

    commit 13 añadiendo controladores de espacio y reservas

---
+++ P01DAW_2022AE650_2023A651_reservas/Controllers/espaciosController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using P01DAW_2022AE650_2023A651_reservas.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace P01DAW_2022AE650_2023A651_reservas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class espaciosController : ControllerBase
+    {
+        private readonly espaciosContext _espaciosContexto;
+
+        public espaciosController(espaciosContext espaciosContexto)
+        {
+            _espaciosContexto = espaciosContexto;
+        }
+    }
+}
---
+++ P01DAW_2022AE650_2023A651_reservas/Controllers/reservasController.cs
@@ -0,0 +1,18 @@
+﻿using P01DAW_2022AE650_2023A651_reservas.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+
+namespace P01DAW_2022AE650_2023A651_reservas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class reservasController : ControllerBase
+    {
+        private readonly espaciosContext _reservasContexto;
+
+        public reservasController(espaciosContext reservasContexto)
+        {
+            _reservasContexto = reservasContexto;
+        }
+    }
+}
