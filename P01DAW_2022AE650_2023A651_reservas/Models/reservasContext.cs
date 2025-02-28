b88d27e831ae14e1f4c37ee57cee9cdfea543337
miche <miche@DESKTOP-EEADCHD>
2025-02-27 19:00:09 -0600

    Commit 11 reservas context

---
+++ P01DAW_2022AE650_2023A651_reservas/Models/reservasContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+namespace P01DAW_2022AE650_2023A651_reservas.Models
+{
+    public class reservasContext : DbContext
+    {
+        public reservasContext(DbContextOptions<reservasContext> options) : base(options)
+        {
+        }
+        public DbSet<reservas> reservas { get; set; }
+    }
+}
