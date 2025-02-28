acbde899317d6d0ee2b16a32b953dd257105ec75
Amaya-2003 <caluchogamer@gmail.com>
2025-02-27 18:05:39 -0600

    commit 3 models con clase usuarios

---
+++ P01DAW_2022AE650_2023A651_reservas/Models/usuarios.cs
@@ -0,0 +1,12 @@
+﻿namespace L01_2022AE650_2023CA651.Models
+{
+    public class usuarios
+    {
+        public int usuarioId { get; set; }
+        public string nombre { get; set; }
+        public string correo { get; set; }
+        public string telefono { get; set; }
+        public string contrasena { get; set; }
+        public string rol { get; set; } 
+    }
+}
