f501edf40969318809a13ac752ade226a958b7b0
miche <miche@DESKTOP-EEADCHD>
2025-02-27 20:42:52 -0600

    Commit 16 controlador con verificacion y actualizacion

--- P01DAW_2022AE650_2023A651_reservas/Controllers/usuariosController.cs
+++ P01DAW_2022AE650_2023A651_reservas/Controllers/usuariosController.cs
@@ -2,6 +2,8 @@
 using Microsoft.AspNetCore.Mvc;
 using P01DAW_2022AE650_2023A651_reservas.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.Data;
+using LoginRequest = P01DAW_2022AE650_2023A651_reservas.Models.LoginRequest;
 
 
 namespace P01DAW_2022AE650_2023A651_reservas.Controllers
@@ -10,10 +12,62 @@ namespace P01DAW_2022AE650_2023A651_reservas.Controllers
     [ApiController]
     public class usuariosController : ControllerBase
     {
-        private readonly usuariosContext _usuariosContext;
+        private readonly usuariosContext _usuariosContexto;
         public usuariosController(usuariosContext usuariosContext)
         {
-            _usuariosContext = usuariosContext;
+            _usuariosContexto = usuariosContext;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<usuarios>> CrearUsuario([FromBody] usuarios nuevoUsuario)
+        {
+            if (nuevoUsuario == null)
+            {
+                return BadRequest("Datos inválidos.");
+            }
+
+
+            _usuariosContexto.usuarios.Add(nuevoUsuario);
+            await _usuariosContexto.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetUsuario), new { id = nuevoUsuario.usuario_id }, nuevoUsuario);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<usuarios>> GetUsuario(int id)
+        {
+            var usuario = await _usuariosContexto.usuarios.FindAsync(id);
+
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+
+            return usuario;
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult> ValidarCredenciales([FromBody] LoginRequest loginRequest)
+        {
+            
+            var usuario = await _usuariosContexto.usuarios
+                .FirstOrDefaultAsync(u => u.correo == loginRequest.Correo);
+
+            if (usuario == null)
+            {
+                return Unauthorized("Usuario no encontrado.");
+            }
+
+            if (loginRequest.Contrasena == usuario.contrasena)
+            {
+                return Ok("Credenciales válidas.");
+            }
+            else
+            {
+                return Unauthorized("Credenciales inválidas.");
+            }
         }
     }
+
+
 }
