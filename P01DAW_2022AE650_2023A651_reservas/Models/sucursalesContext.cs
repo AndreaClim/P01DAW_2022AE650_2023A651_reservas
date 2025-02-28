0c1692acc2c3e5c52fe837996ff9a36e739db17b
Amaya-2003 <caluchogamer@gmail.com>
2025-02-27 19:28:19 -0600

    commit usuarios y sucursales context

---
+++ P01DAW_2022AE650_2023A651_reservas/Models/sucursalesContext.cs
@@ -0,0 +1,14 @@
+﻿using L01_2022AE650_2023CA651.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+namespace P01DAW_2022AE650_2023A651_reservas.Models
+{
+    public class sucursalesContext : DbContext
+    {
+        public sucursalesContext(DbContextOptions<sucursalesContext> options) : base(options) 
+        {
+
+        }
+        public DbSet<sucursales> sucursales { get; set; }
+    }
+}
---
+++ P01DAW_2022AE650_2023A651_reservas/Models/usuariosContext.cs
@@ -0,0 +1,14 @@
+﻿using L01_2022AE650_2023CA651.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+namespace P01DAW_2022AE650_2023A651_reservas.Models
+{
+    public class usuariosContext : DbContext
+    {
+        public usuariosContext(DbContextOptions<usuariosContext> options) : base(options)
+        {
+
+        }
+        public DbSet<usuarios> usuarios { get; set; }
+    }
+}
